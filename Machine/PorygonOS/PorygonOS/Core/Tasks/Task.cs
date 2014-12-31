9c2e0760d2cf8368135c1eb13b0a870d8ff451d0
Benny Peake <bpeake@wpi.edu>
2014-12-30 21:59:34 -0800

    Created project Added Log class for simple debugging Added TaskThread multitasking system to auto distribute tasks between threads as needed

---
+++ Machine/PorygonOS/PorygonOS/Core/Tasks/Task.cs
@@ -0,0 +1,44 @@
+﻿using PorygonOS.Core.Debug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorygonOS.Core.Tasks
+{
+    /// <summary>
+    /// A task to be run
+    /// </summary>
+    public abstract class Task
+    {
+        /// <summary>
+        /// Are we shutting down
+        /// </summary>
+        protected bool IsShuttingDown
+        {
+            get { return bShutdown; }
+        }
+
+        /// <summary>
+        /// Called every time this task should run
+        /// </summary>
+        /// <returns>The number of milliseconds to wait before running again, or <= 0 to stop</returns>
+        public int Run()
+        {
+            Log.WriteLine("Running {0}.", GetType().Name);
+            int waitTime = OnRun();
+            Log.WriteLine("Finished {0}.", GetType().Name);
+            return waitTime;
+        }
+
+        public void Shutdown()
+        {
+            bShutdown = true;
+        }
+
+        protected abstract int  OnRun();
+
+        private bool bShutdown;
+    }
+}
