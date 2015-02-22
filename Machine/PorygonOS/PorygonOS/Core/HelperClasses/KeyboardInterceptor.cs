01f7d71a66bf17e249e5cb0514573b25dc5c967b
Evan Polekoff <eapolekoff@wpi.edu>
2015-02-22 01:43:26 -0500

    Issue #10
    The KeyboardInterceptor now blocks all keys that are not in the GlobalConfig. It remaps the pressed key to a new key from the game config file (currently set to null, but will be passed in later).

--- Machine/PorygonOS/PorygonOS/Core/HelperClasses/KeyboardInterceptor.cs
+++ Machine/PorygonOS/PorygonOS/Core/HelperClasses/KeyboardInterceptor.cs
@@ -12,11 +12,16 @@ namespace PorygonOS.Core.HelperClasses
 {
     class KeyboardInterceptor
     {
+        //Variables for setting up the hook
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
 
+        //Variables for keeping track of the hardmapped keys and the keys from the config
+        private static string keyMapConfigSection = "KeyMapping";
+        private static string[] keyMapConfigKeys = new string[]{ "green1", "red1", "blue1", "yellow1", "white1", "black1",
+                                                  "green2", "red2", "blue2", "yellow2", "white2", "black2" };
 
         /// <summary>
         /// Start intercepting the keys.
@@ -24,9 +29,28 @@ namespace PorygonOS.Core.HelperClasses
         public void Start()
         {
             _hookID = SetHook(_proc);
+            RemapKeys(null);
             Application.Run();
 
             UnhookWindowsHookEx(_hookID);
+        }
+
+        /// <summary>
+        /// Populate a dictionary of hard-mapped keys to game-sepcific keys
+        /// </summary>
+        public static Dictionary<string, string> RemapKeys(Config.ConfigFile gameConfigFile)
+        {
+            Dictionary<string, string> remapDictionary = new Dictionary<string, string>();
+
+            //Loop over every key
+            for (int i = 0; i < keyMapConfigKeys.Length; i ++ )
+            {
+                //Map the global config key to the game config key
+                remapDictionary.Add(Program.GlobalConfig.GetString(keyMapConfigSection, keyMapConfigKeys[i]), "G");//Until we are able to properly load the gameconfig, just type "G"
+                                    //gameConfigFile.GetString(keyMapConfigSection, keyMapConfigKeys[i]));
+            }
+
+            return remapDictionary;
         }
 
 
@@ -54,12 +78,26 @@ namespace PorygonOS.Core.HelperClasses
         private static IntPtr HookCallback(
             int nCode, IntPtr wParam, IntPtr lParam)
         {
+
+            //Load the game config file.
+            Config.ConfigFile gameConfigFile = null;
+            //Get the dictionary of keys from the configs
+            Dictionary<string, string> keyDictionary = RemapKeys(gameConfigFile);
+
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
                 //Get the key that was pressed.
                 int vkCode = Marshal.ReadInt32(lParam);
-                Console.WriteLine((Keys)vkCode);
 
+                //Parse the key mapping out of the config files.
+                Keys remappedKey;
+                string pressedKeyString = ((Keys)vkCode).ToString();
+                if(keyDictionary.ContainsKey(pressedKeyString))
+                {
+                    Enum.TryParse(keyDictionary[pressedKeyString], out remappedKey);
+                    Console.WriteLine(remappedKey);
+                }
+                
                 //Replace the pressed key with a new key and pass it along
                 KBDLLHOOKSTRUCT replacementKey = new KBDLLHOOKSTRUCT();
                 Marshal.PtrToStructure(lParam, replacementKey);
