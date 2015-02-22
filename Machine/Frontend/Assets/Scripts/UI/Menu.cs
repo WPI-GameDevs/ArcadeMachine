c1a8da659088ba2a97df36aa2209a0fd0496020e
higoset <ejcerini@wpi.edu>
2015-02-21 23:38:28 -0500

    Issue #13 Added code to Menu.cs
    The added code handles adding and removing (Pushing and Popping) items from the Pseudo-stack and adds the OnPush and OnPop functions that will run at the beginning of the Push and Pop functions and allow Parent classes to overwrite them with their own definitions.
    
    Tested By: Currently Untested.
    
    Reviewer @bpeake13

--- Machine/Frontend/Assets/Scripts/UI/Menu.cs
+++ Machine/Frontend/Assets/Scripts/UI/Menu.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic
 
 public class Menu : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +13,34 @@ public class Menu : MonoBehaviour {
 	void Update () {
 	
 	}
+	
+	//Function for adding a Menu to the global stack!~
+	void Push(List<Menu> menulist){
+		OnPush();
+		if(menulist.Contains(this)){
+			menulist.Remove(this);
+		}
+		menulist.add(this);
+	}
+	
+	//Function for removing a Menu from the global stack!~
+	void PopInstance(List<Menu> menulist){
+		OnPop();
+		menulist.Remove(this);
+	}
+	
+	//Function for removing the Menu at the end of the global stack!!~
+	static void Pop(List<Menu> menulist){
+		OnPop();
+		Menu temp = menulist.last();
+		menulist.Remove(temp);
+	}
+	
+	//Functions to be overridden by parent classes!~
+	virtual void OnPop(){
+	}
+	
+	virtual void OnPush(){
+	}
+	
 }
